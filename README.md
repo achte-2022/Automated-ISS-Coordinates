01c38211be6b1737757909d7211d949467673f4f
Tejas Acharya <106873307+achte-2022@users.noreply.github.com>
2022-06-22 20:40:39 -0700

    Update

---
+++ README.md
@@ -0,0 +1,37 @@
+# Automated-ISS-Coordinates
+
+### [twitter](https://twitter.com/achte_te)
+
+## Description
+An application that checks the realtime coordinates of International Space Station. The coordinates can be checked at [coordinate_checker](https://www.latlong.net/Show-Latitude-Longitude.html).
+
+If the ISS is above the specified location coordinates and if the sun has set in the location on the ground, an email is sent using the SMTP protocol.  
+## Requirements
+
+[Python](https://www.python.org/)
+
+```sh
+$ python3 --version
+Python 3.9.12
+```
+
+[requests](https://pypi.org/project/requests/)
+
+[datetime](https://docs.python.org/3/library/datetime.html)
+
+[ISS API](http://open-notify.org/Open-Notify-API/ISS-Location-Now/)
+
+[SUNSET-SUNRISE-API](https://sunrise-sunset.org/api)
+
+## Install
+
+```sh
+$ git clone git@github.com:achte-2022/ISS-Coordinates.git
+```
+
+## Run
+
+```sh
+$ cd ISS-Coordinates
+$ python3 main.py
+```
---
+++ main.py
@@ -0,0 +1,73 @@
+# IMPORTING LIBRARIES
+import requests
+import datetime as dt
+import time
+import smtplib
+
+# CONSTANTS
+ISS_API_ENDPOINT = "http://api.open-notify.org/iss-now.json"
+SUNSET_SUNRISE_API_ENDPOINT = "https://api.sunrise-sunset.org/json"
+DEGREE_OFFSET = 5
+
+# DUMMY COORDINATES
+MY_LATITUDE = -134.052235
+MY_LONGITUDE = 18.243683
+
+# DUMMY CREDENTIALS
+FROM_SMTP_SERVER = "smtp.mail.yahoo.com"
+FROM_MAIL = "random@yahoo.com"
+TO_MAIL = "random@gmail.com"
+SUBJECT = "Subject:Lookout for ISS.\n"
+PASSWORD = "password123"
+
+# GET ISS REQUEST
+iss_response = requests.get(ISS_API_ENDPOINT)
+iss_response.raise_for_status()
+
+# ISS JSON DATA
+iss_data = iss_response.json()
+iss_latitude = float(iss_data["iss_position"]["latitude"])
+iss_longitude = float(iss_data["iss_position"]["longitude"])
+
+while True:
+    time.sleep(60)
+    if (abs(iss_latitude - MY_LATITUDE) <= DEGREE_OFFSET) and (
+        abs(iss_longitude - MY_LONGITUDE) <= DEGREE_OFFSET
+    ):
+
+        sunset_sunrise_parameters = {
+            "lat": MY_LATITUDE,
+            "lng": MY_LONGITUDE,
+            "formatted": 0,
+        }
+
+        # GET SUNSET-SUNRISE(SS) REQUEST
+        ss_response = requests.get(
+            SUNSET_SUNRISE_API_ENDPOINT, params=sunset_sunrise_parameters
+        )
+        ss_response.raise_for_status()
+
+        #  SS JSON DATA
+        ss_data = ss_response.json()
+
+        sunrise = ss_data["results"]["sunrise"]
+        sunset = ss_data["results"]["sunset"]
+
+        sunrise_hour = int(sunrise.split("T")[1].split(":")[0])
+        sunset_hour = int(sunset.split("T")[1].split(":")[0])
+
+        current_hour = dt.datetime.now().hour
+
+        message = SUBJECT + "ISS is above you. Go out and check the night sky."
+
+        if current_hour > sunset_hour or current_hour < sunrise:
+            try:
+                with smtplib.SMTP(FROM_SMTP_SERVER) as connection:
+                    connection.starttls()
+                    connection.login(user=FROM_MAIL, password=PASSWORD)
+                    connection.sendmail(
+                        from_addr=FROM_MAIL, to_addrs=TO_MAIL, msg=message
+                    )
+                print("Mail sent.")
+            except:
+                print("Mail could not be sent.")
